a2fcb5ba8fe06dbe0727368f2c35777ae1c0aa44
jgough <joe.gough@jitsuin.com>
2022-11-10 15:37:49 +0000

    Read events via generator
    Problem:
    reading all events into a list would consume too
    much memory.
    
    Solution:
    use new order_by selector to read events individually and
    accumulate the bencoded data into a hash.
    
    signed-off-by: Paul Hewlett <phewlett76@gmail.com>
    signed-off-by: Joe Gough <joegough97@gmail.com>
    signed-off-by: Robin Bryce <rbryce@gmail.com>

---
+++ unittests/testgetauthtoken.py
@@ -0,0 +1,65 @@
+"""
+Test get_auth_token method
+"""
+
+from unittest import TestCase, mock
+
+from rkvst_simplehash.v1 import get_auth_token
+
+from .mock_response import MockResponse
+
+FQDN = "app.rkvst-test.io"
+CLIENT_ID = "client_id-2f78-4fa0-9425-d59314845bc5"
+CLIENT_SECRET = "client_secret-388f5187e32d930d83"
+ACCESS_TOKEN = "access_token-xbXATAWrEpepR7TklOxRB-yud92AsD6DGGasiEGN7MZKT0AIQ4Rw9s"
+REQUEST = {
+    "grant_type": "client_credentials",
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+}
+
+RESPONSE = {
+    "access_token": ACCESS_TOKEN,
+    "expires_in": 660,
+    "token_type": "Bearer",
+}
+
+
+class TestGetAuthToken(TestCase):
+    """
+    Test get_auth_token
+    """
+
+    maxDiff = None
+
+    @mock.patch("rkvst_simplehash.v1.requests_post")
+    def test_get_auth_token_v1(self, mock_post):
+        """
+        Test anchor_events
+        """
+
+        mock_post.return_value = MockResponse(200, **RESPONSE)
+        token = get_auth_token(
+            FQDN,
+            CLIENT_ID,
+            CLIENT_SECRET,
+        )
+        args, kwargs = mock_post.call_args
+        self.assertEqual(
+            args,
+            ("https://app.rkvst-test.io/archivist/iam/v1/appidp/token",),
+            msg="CREATE method args called incorrectly",
+        )
+        self.assertEqual(
+            kwargs,
+            {
+                "data": REQUEST,
+                "timeout": 10,
+            },
+            msg="CREATE method kwargs called incorrectly",
+        )
+        self.assertEqual(
+            token,
+            ACCESS_TOKEN,
+            msg="TOKEN method called incorrectly",
+        )
--- unittests/testredactevents.py
+++ unittests/testredactevents.py
@@ -6,10 +6,10 @@ from unittest import TestCase
 
 
 from rkvst_simplehash.v1 import (
-    redact_events,
+    redact_event,
 )
 
-from .testhashevents import VALID_EVENTS
+from .testanchorevents import VALID_EVENT
 
 REDACTED_EVENT = {
     "identity": (
@@ -45,62 +45,21 @@ REDACTED_EVENT = {
     "from": "0x92a2923C46ff3904f8Fb0E80d1f0448AeE10BC9d",
 }
 
-REDACTED_EVENT1 = {
-    "identity": (
-        "assets/64dc9f7a-9d94-4ee0-835e-34567e882c9e/"
-        "events/1234825a-a62e-459a-b091-4571cd7b467e"
-    ),
-    "asset_identity": "assets/64dc9f7a-9d94-4ee0-835e-34567e882c9e",
-    "behaviour": "RecordEvidence",
-    "operation": "Record",
-    "confirmation_status": "CONFIRMED",
-    "asset_attributes": {},
-    "event_attributes": {
-        "arc_description": "Safety conformance approved for version 1.6.",
-        "arc_evidence": "DVA Conformance Report attached",
-        "conformance_report": "blobs/e2a1d16c-03cd-45a1-8cd0-547189df1273",
-    },
-    "principal_accepted": {
-        "display_name": "stunt-idp@jitsuin.com",
-        "email": "stunt-idp@jitsuin.com",
-        "issuer": "stunt-idp@jitsuin.com",
-        "subject": "stunt-idp@jitsuin.com",
-    },
-    "principal_declared": {
-        "display_name": "",
-        "email": "phil.b@synsation.io",
-        "issuer": "idp.synsation.io/1234",
-        "subject": "phil.b",
-    },
-    "tenant_identity": "tenant/0dc854f5-5eab-4ad8-9d75-1ea9b24d0db4",
-    "timestamp_accepted": "2022-10-14T09:30:52Z",
-    "timestamp_committed": "2022-10-14T09:30:52.920717507Z",
-    "timestamp_declared": "2019-11-28T14:44:19Z",
-    "from": "0x92a2923C46ff3904f8Fb0E80d1f0448AeE10BC9d",
-}
 
-
-# should be sorted by identity
-REDACTED_EVENTS = [
-    REDACTED_EVENT1,
-    REDACTED_EVENT,
-]
-
-
-class TestRedactEventsV1(TestCase):
+class TestRedactEventV1(TestCase):
     """
-    Test redacted_events
+    Test redacted_event
     """
 
     maxDiff = None
 
-    def test_redact_events_v1(self):
+    def test_redact_event_v1(self):
         """
         Test redacted_events
         """
-        redacted_events = redact_events(VALID_EVENTS["events"])
+        redacted_event = redact_event(VALID_EVENT)
         self.assertEqual(
-            REDACTED_EVENTS,
-            redacted_events,
+            REDACTED_EVENT,
+            redacted_event,
             msg="redacted_events has incorrect order",
         )
