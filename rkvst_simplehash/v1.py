882e7d9b340f7ba8a79c0c675ef677cd604f0ce8
jgough <joe.gough@jitsuin.com>
2022-11-10 15:37:49 +0000

    first update

--- rkvst_simplehash/v1.py
+++ rkvst_simplehash/v1.py
@@ -9,6 +9,8 @@ from sys import stdin as sys_stdin
 
 from bencodepy import encode as binary_encode
 
+import requests
+
 V1_FIELDS = {
     "identity",
     "asset_identity",
@@ -27,6 +29,9 @@ V1_FIELDS = {
 }
 
 
+class ArchivistBadFieldError(Exception):
+    """Incorrect field name in list() method"""
+
 class SimpleHashPendingEventFound(Exception):
     """If PENDING event found"""
 
@@ -35,42 +40,94 @@ class SimpleHashFieldMissing(Exception):
     """If essential field is missing"""
 
 
-def __check_events(events):
+def __check_event(event):
     """Raise exception if any PENDING events found or
     if required keys are missing"""
 
-    for event in events:
-        missing = V1_FIELDS.difference(event)
-        if missing:
-            raise SimpleHashFieldMissing(
-                f"Event Identity {event['identity']} has missing field(s) {missing}"
-            )
-        if event["confirmation_status"] not in ("FAILED", "CONFIRMED"):
-            raise SimpleHashPendingEventFound(
-                f"Event Identity {event['identity']} has illegal "
-                f"confirmation status {event['confirmation_status']}"
-            )
+    missing = V1_FIELDS.difference(event)
+    if missing:
+        raise SimpleHashFieldMissing(
+            f"Event Identity {event['identity']} has missing field(s) {missing}"
+        )
+    if event["confirmation_status"] not in ("FAILED", "CONFIRMED"):
+        raise SimpleHashPendingEventFound(
+            f"Event Identity {event['identity']} has illegal "
+            f"confirmation status {event['confirmation_status']}"
+        )
 
 
-def redact_events(events):
-    """Form list of sorted (by identity) events only containing necessary fields"""
-    return [
-        {k: event[k] for k in V1_FIELDS}
-        for event in sorted(events, key=itemgetter("identity"))
-    ]
+def redact_event(event):
+    """Form an event only containing necessary fields"""
+    return  {k: event[k] for k in V1_FIELDS}
 
 
-def hash_events(response):
+def list_events(start_time, end_time, auth_token):
+        """GET method (REST) with params string
+        Lists events that match the params dictionary.
+        If page size is specified return the list of records in batches of page_size
+        until next_page_token in response is null.
+        If page size is unspecified return up to the internal limit of records.
+        (different for each endpoint)
+        Args:
+            start_time (string): rfc3339 formatted datetime string of the start date of the time window of events
+            end_time (string): rfc3339 formatted datetime string of the end date of the time window of events
+            auth_token (string): authorization token to be able to call the list events api
+        Returns:
+            iterable that lists events
+        Raises:
+            ArchivistBadFieldError: field has incorrect value.
+        """
+
+        url = "https://app.rkvst.io/archivist/v2/assets/-/events"
+        params = {
+            "proof_mechanism": "SIMPLEHASH",
+            "start_time": start_time,
+            "end_time": end_time,
+            "page_size": 10,
+            "order_by": "SIMPLEHASHV1"
+        }
+        headers = {
+            "Authorization", f"Bearer {auth_token}"
+        }
+
+        while True:
+            response = requests.get(url, params=params, headers=headers)
+            data = response.json()
+
+            try:
+                events = data["events"]
+            except KeyError as ex:
+                raise ArchivistBadFieldError(f"No events found") from ex
+
+            for event in events:
+                yield event
+
+            page_token = data.get("next_page_token")
+            if not page_token:
+                break
+
+            params = {"page_token": page_token}
+
+
+def hash_events(start_time, end_time, auth_token):
     """Generate Simplehash for a given set of events canonicalizing then hashing"""
 
-    __check_events(response["events"])
-    redacted_events = redact_events(response["events"])
+    hasher = sha256()
 
-    # bencode the events which produces the canonical form
-    canonicalized_events = binary_encode(redacted_events)
+    # for each event
+    for event in list_events(start_time, end_time, auth_token):
 
-    # Hash the canonicalized events
-    return sha256(canonicalized_events).hexdigest()
+        __check_event(event)
+        redacted_event = redact_event(event)
+
+        # bencode the events which produces the canonical form
+        bencoded_event = binary_encode(redacted_event)
+
+        # add the event to the sha256 hash
+        hasher.update(bencoded_event)
+
+    # return the complete hash
+    return hasher.hexdigest()
 
 
 def main():
